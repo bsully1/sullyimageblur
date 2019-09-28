e939d807ed863cadf01a06c126ab5f1878d3bcc4
Brandon Sullivan <jbs81890@gmail.com>
2019-09-28 19:48:30 +0000

    Completed

--- imageb.rb
+++ imageb.rb
@@ -1,3 +1,16 @@
+class Image
+  def initialize(image_array)
+    @image_array = image_array
+  end
+
+  def output_image
+    @image_array.each do |image_row|
+      puts image_row.join
+  end
+
+  end
+
+end
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
@@ -6,6 +19,10 @@ image = Image.new([
 ])
 image.output_image
 
-class Image
-
-end
+food = Image.new([
+  [1, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 1, 1],
+  [0, 0, 0, 1]
+])
+food.output_image
