dbaec3e857f806fee87fc3dcaa6c13edcfbb723a
Brandon Sullivan <jbs81890@gmail.com>
2019-09-28 18:00:51 +0000

    initial commit

---
+++ card.rb
@@ -0,0 +1,36 @@
+class Card
+  attr_accessor :rank, :suit
+  
+    def initialize(rank, suit) 
+      @rank = rank
+      @suit = suit
+    end
+
+    def output_card
+      puts "#{self.rank} of #{self.suit}"
+    end
+
+end
+
+class Deck
+  def initialize
+    @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
+    @suits = ['♣', '♥', '♠', '♦']
+    @cards = []
+ 
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(rank, suit)
+      end
+    end
+  
+  @cards.shuffle!
+  end
+ 
+  def deal (number)
+    number.times {@cards.shift.output_card}
+  end
+end
+
+deck = Deck.new
+deck.deal(1)
---
+++ imageb.rb
@@ -0,0 +1,11 @@
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
+
+class Image
+
+end
